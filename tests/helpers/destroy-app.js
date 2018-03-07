dc8800cc8fe02863e69e44c0ee4491f91ba3a884
zahar4uk <zahar4uk.vladislaw2016@yandex.ru>
2018-03-07 13:40:22 +0200

    Revert "Creating Model Products. Added ember-cli-mirage"
    This reverts commit dc7a2fbfde07a81bfe2e79d3855e7fd0787911f0.

--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -2,7 +2,4 @@ import { run } from '@ember/runloop';
 
 export default function destroyApp(application) {
   run(application, 'destroy');
-  if (window.server) {
-    window.server.shutdown();
-  }
 }
